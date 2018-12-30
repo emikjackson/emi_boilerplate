a43f5d1c799b77ad2e2d0422a36e9473bb6bfa6c
David Kooistra <dkooi@umich.edu>
2018-12-30 11:22:41 -0500

    Text input working for login

--- client/components/Pages/Login.js
+++ client/components/Pages/Login.js
@@ -5,13 +5,26 @@ class Login extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-
-		}
+			username: '',
+			password: '',
+		};
 
 		// Bind the functions
+		this.handleChange = this.handleChange.bind(this);
+    	this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
 	// functions
+	handleChange(event) {
+		this.setState({
+			[event.target.name]: event.target.value
+		});
+	}
+	
+	handleSubmit(event) {
+		alert('username: ' + this.state.username + '\npassword:' + this.state.password);
+		event.preventDefault();
+	}
 
 	render() {
 		return(
@@ -20,8 +33,21 @@ class Login extends Component {
                 <div className="lgn-table">
 					<div className="logo"></div>
 					<div className="text-box-container">
-						<div className="text-box"></div>
-						<div className="text-box"></div>
+						<form onSubmit={this.handleSubmit}>
+							<div className="text-box">
+								<label>
+									Username:
+									<input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
+								</label>
+							</div>
+							<div className="text-box">
+								<label>
+									Password:
+									<input type="text" name="password" value={this.state.password} onChange={this.handleChange} />
+								</label>
+							</div>
+							<input type="submit" value="Submit" />
+						</form>
 					</div>
 				</div>
 			</div>
